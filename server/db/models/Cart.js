bf61d1996d76676981bed62bc480791a44e151fa
Ellie Ly <elliel.4797@gmail.com>
2022-01-12 14:33:42 -0500

    Defined association between user and product through a cart join table

--- server/db/models/Cart.js
+++ server/db/models/Cart.js
@@ -1,8 +1,16 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+const Sequelize = require('sequelize');
+const db = require('../db');
 
-const Cart = db.define('cart',{
+const Cart = db.define('cart', {});
 
-})
+module.exports = Cart;
 
-module.exports = Cart
+Cart.prototype.priceTotal = function () {
+	//Return the sum price of all items in cart
+};
+
+//Methods for deleteing relationship between user and product
+//AKA When you remove a product from the cart
+
+//Method for creating relationship between user and product
+//AKA when you add a product to the cart
