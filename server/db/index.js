d6e0725e0de4dd237301bf1345d120e916a8114e
Quynh Le <nhuqxl@gmail.com>
2022-01-13 15:59:45 -0800

    updated branch with main changes

--- server/db/index.js
+++ server/db/index.js
@@ -2,13 +2,6 @@
 
 const db = require('./db')
 
-<<<<<<< HEAD
-const User = require('./models/User')
-const Product = require('./models/Product')
-const Cart = require('./models/Cart')
-//associations could go here!
-
-=======
 const User = require('./models/User');
 const Product = require('./models/Product');
 const Order = require('./models/Order')
@@ -22,22 +15,12 @@ User.hasMany(Order)
 Order.belongsTo(User)
 
 /*
->>>>>>> main
 User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.hasMany(Product);
 Product.belongsTo(Cart);
 
 module.exports = {
-<<<<<<< HEAD
-  db,
-  models: {
-    User,
-    Product,
-    Cart
-  },
-}
-=======
 	db,
 	models: {
 		User,
@@ -46,4 +29,3 @@ module.exports = {
 		Cart_Item
 	},
 };
->>>>>>> main
