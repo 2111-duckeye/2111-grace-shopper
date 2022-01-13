d17d926ee6726db1b908e0182eb489b11ecc0f9d
Ellie Ly <elliel.4797@gmail.com>
2022-01-13 13:01:38 -0500

    Added Feature: Security - filter get request for user to send spepecific attributes

--- server/api/users.js
+++ server/api/users.js
@@ -1,37 +1,31 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const {
-  models: { User },
-} = require("../db");
-const { requireToken, isAdmin } = require("./gatekeepingMiddleware");
+	models: { User },
+} = require('../db');
+const { requireToken, isAdmin } = require('./gatekeepingMiddleware');
 module.exports = router;
 
-router.get("/", requireToken, isAdmin, async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ["id", "username"],
-    });
-    res.json(users);
-  } catch (err) {
-    next(err);
-  }
+router.get('/', requireToken, isAdmin, async (req, res, next) => {
+	try {
+		const users = await User.findAll({
+			// explicitly select only the id and username fields - even though
+			// users' passwords are encrypted, it won't help if we just
+			// send everything to anyone who asks!
+			attributes: ['id', 'username'],
+		});
+		res.json(users);
+	} catch (err) {
+		next(err);
+	}
 });
 
-router.post("/", async (req, res, next) => {
-  try {
-    //Go through JWT and BCRYPT of tokenizing the password before putting it into the database
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get("/:userId", async (req, res, next) => {
-  try {
-    const singleUser = await User.findByPk(req.params.userId);
-    res.json(singleUser);
-  } catch (err) {
-    next(err);
-  }
+router.get('/:userId', async (req, res, next) => {
+	try {
+		const singleUser = await User.findByPk(req.params.userId, {
+			attributes: ['id', 'username'],
+		});
+		res.json(singleUser);
+	} catch (err) {
+		next(err);
+	}
 });
