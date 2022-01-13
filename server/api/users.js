6cb85038401febba8e536f37b4076a6b4fcbed6f
Quynh Le <nhuqxl@gmail.com>
2022-01-13 15:57:38 -0800

    updated branch with main changes

--- server/api/users.js
+++ server/api/users.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
-
-router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
-
-router.post('/',async (req,res,next) =>{
-  try{
-    //Go through JWT and BCRYPT of tokenizing the password before putting it into the database
-  } catch(err){
-    next(err)
-  }
-})
-
-router.get('/:userId', async (req,res,next) => {
-  try{
-      const singleUser = await User.findByPk(req.params.userId);
-      res.json(singleUser);
-  } catch(err){
-      next(err)
-  }
-})
-=======
 const router = require('express').Router();
 const {
 	models: { User },
@@ -65,4 +29,3 @@ router.get('/:userId', async (req, res, next) => {
 		next(err);
 	}
 });
->>>>>>> main
