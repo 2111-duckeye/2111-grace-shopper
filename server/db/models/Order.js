80f113cd2b4b43afb339603ca8f5f3823fd217ed
quynhl3 <75707023+quynhl3@users.noreply.github.com>
2022-01-13 12:18:05 -0800

    removed products in order model

--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -6,10 +6,6 @@ const Order = db.define('order',{
         type: Sequelize.BOOLEAN,
         defaultValue: false
     },
-    product:{
-        type: Sequelize.ARRAY(Sequelize.TEXT),
-        deafultValue: []
-    },
     total:{
         type: Sequelize.DECIMAL(10, 2),
         defaultValue: 0
