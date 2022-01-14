69da790a1f4ac8743554929d5b99ff59766a5ad0
Ellie Ly <elliel.4797@gmail.com>
2022-01-14 17:00:15 -0500

    Fix: Gatekeeping middleware for get request on all user now working

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../db/models/user')
+const User = require('../db/models/user');
 const { requireToken, isAdmin } = require('./gatekeepingMiddleware');
 module.exports = router;
 
