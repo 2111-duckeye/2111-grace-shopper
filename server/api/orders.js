d1d406f06e08e2785f0309efc73d2f6b32ee6e57
Melissa Moy <melissa.e.moy@gmail.com>
2022-01-14 14:03:30 -0500

    feat: add eager loading of associated products to order

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const {
-	models: { Order },
+	models: { Order, Product },
 } = require('../db');
 //const { requireToken, isAdmin } = require('./gatekeepingMiddleware');
 module.exports = router;
 
 router.get('/', async (req, res, next) => {
 	try {
-		const orders = await Order.findAll();
+		const orders = await Order.findAll({
+			include: Product
+		});
 		res.json(orders);
 	} catch (err) {
 		next(err);
@@ -17,6 +19,7 @@ router.get('/', async (req, res, next) => {
 router.get('/:orderId', async (req, res, next) => {
 	try {
 		const currentOrder = await Order.findOne({
+			include: Product,
       where: { id: req.params.orderId }
     });
 		res.json(currentOrder);
@@ -28,6 +31,7 @@ router.get('/:orderId', async (req, res, next) => {
 router.get('/user/:userId', async (req, res, next) => {
 	try {
 		const orders = await Order.findAll({
+			include: Product,
       where: { userId: req.params.userId }
     });
 		res.json(orders);
@@ -39,6 +43,7 @@ router.get('/user/:userId', async (req, res, next) => {
 router.get('/user/:userId/open/', async (req, res, next) => {
 	try {
 		const order = await Order.findOne({
+			include: Product,
       where: {
 				userId: req.params.userId,
 				completed: false
