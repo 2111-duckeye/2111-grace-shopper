ad2745971f5e5511b59da28f557d93869cd5c024
Yooboo Park <yooboopark@Yooboos-Air.home>
2022-01-13 21:37:40 -0600

    Products fetch,create,delete action/thunk/reducer created

--- client/store/index.js
+++ client/store/index.js
@@ -4,8 +4,9 @@ import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import auth from './auth'
 import users from './users'
+import products from './products'
 
-const reducer = combineReducers({ auth, users })
+const reducer = combineReducers({ auth, users, products })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 )
@@ -14,3 +15,4 @@ const store = createStore(reducer, middleware)
 export default store
 export * from './auth'
 export * from './users'
+export * from './products'
